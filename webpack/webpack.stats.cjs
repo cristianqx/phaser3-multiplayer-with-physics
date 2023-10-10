ae0bca284ca9afaa5be4b1c9303a34c2b6ffb3b2
Cristian Baptistella <cristian.baptistella@outlook.com>
2023-10-10 09:27:59 -0300

    first commit

---
+++ webpack/webpack.stats.cjs
@@ -0,0 +1,45 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  stats: 'errors-warnings',
+  entry: ['./src/stats/index.ts'],
+  output: {
+    publicPath: 'static/stats',
+    path: path.resolve(__dirname, '../dist/stats'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+          filename: '[name].bundle.js'
+        }
+      }
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/stats/index.html'
+    })
+  ]
+}
